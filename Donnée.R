c22f4dd71666f2a5023f8ca01e04e3f11b7262ce
MaximeDESBOIS <max7410@hotmail.fr>
2018-02-20 18:30:22 +0100

    First commit

---
+++ Donnée.R
@@ -0,0 +1,62 @@
+#transforme la liste des target en vecteur
+targetCol <- unlist(Train["target"], use.names=FALSE)
+#ou
+targetCol <- Train$target
+
+#moyenne des target
+mean(targetCol)#0.03644
+
+#nombre de 1 dans target
+length(targetCol[targetCol==1]) #21694
+#nombre de 0 dans target
+length(targetCol[targetCol==0]) #573518
+
+
+#moyenne sans avoir les -1 cad NA
+mean(Train$ps_ind_02_cat[Train$ps_ind_02_cat>(-1)])
+
+summary(Train$ps_ind_02_cat[Train$ps_ind_02_cat>(-1)])
+summary(Train) 
+
+
+
+Train %>% ggvis(~ps_ind_01, ~ps_ind_03, fill = ~target) %>% layer_points()
+
+# Percentual division of `Species`
+round(prop.table(table(iris$Species)) * 100, digits = 1)
+
+# Division of `Species`
+table(Train$target)
+
+table(Train$ps_ind_02_cat[Train$ps_ind_02_cat>(-1)])
+
+str(Train)
+
+#alt+c pour commenter
+
+# Handling imbalanced classes As we mentioned above the proportion of records
+# with target=1 is far less than target=0. This can lead to a model that has
+# great accuracy but does have any added value in practice. Two possible
+# strategies to deal with this problem are:
+# 
+# oversampling records with target=1 undersampling records with target=0
+# 
+# There are many more strategies of course and MachineLearningMastery.com gives
+# a nice overview. As we have a rather large training set, we can go for
+# undersampling.
+
+# ps_car_03_cat and ps_car_05_cat have a large proportion of records with missing values. Remove these variables.
+# For the other categorical variables with missing values, we can leave the missing value -1 as such.
+# ps_reg_03 (continuous) has missing values for 18% of all records. Replace by the mean.
+# ps_car_11 (ordinal) has only 5 records with misisng values. Replace by the mode.
+# ps_car_12 (continuous) has only 1 records with missing value. Replace by the mean.
+# ps_car_14 (continuous) has missing values for 7% of all records. Replace by the mean.
+
+#correlation
+Train %>%
+  select(-starts_with("ps_calc"), -ps_ind_10_bin, -ps_ind_11_bin, -ps_car_10_cat, -id) %>%
+  mutate_at(vars(ends_with("cat")), funs(as.integer)) %>%
+  mutate_at(vars(ends_with("bin")), funs(as.integer)) %>%
+  mutate(target = as.integer(target)) %>%
+  cor(use="complete.obs", method = "spearman") %>%
+  corrplot(type="lower", tl.col = "black",  diag=FALSE)
---
+++ lib.R
@@ -0,0 +1,65 @@
+install.packages("scales")
+install.packages("ggplot2")
+install.packages("grid")
+install.packages("ggthemes")
+install.packages("gridExtra")
+install.packages("RColorBrewer")
+install.packages("corrplot")
+
+install.packages("dplyr")
+install.packages("readr")
+install.packages("data.table")
+install.packages("tibble")
+install.packages("tidyr")
+install.packages("stringr")
+install.packages("forcats")
+install.packages("rlang")
+install.packages("alluvial")
+#install.packages("ggfortify")
+install.packages("ggrepel")
+install.packages("ggridges")
+install.packages("VIM")
+install.packages("plotly")
+install.packages("ggforce")
+install.packages("caret")
+install.packages("MLmetrics")
+
+
+# general visualisation
+library(ggplot2) # visualisation
+library('scales') # visualisation
+library('grid') # visualisation
+library('ggthemes') # visualisation
+library('gridExtra') # visualisation
+library('RColorBrewer') # visualisation
+library('corrplot') # visualisation
+
+# general data manipulation
+library('dplyr') # data manipulation
+library('readr') # input/output
+library('data.table') # data manipulation
+library('tibble') # data wrangling
+library('tidyr') # data wrangling
+library('stringr') # string manipulation
+library('forcats') # factor manipulation
+library('rlang') # data manipulation
+
+# specific visualisation
+library('alluvial') # visualisation
+#library('ggfortify') # visualisation
+library('ggrepel') # visualisation
+library('ggridges') # visualisation
+library('VIM') # NAs
+library('plotly') # interactive
+library('ggforce') # visualisation
+
+# modelling
+library('xgboost') # modelling
+library('caret') # modelling
+library('MLmetrics') # gini metric
+
+install.packages("ggvis")
+library(ggvis)
+
+install.packages("BBmisc")
+library(BBmisc)
